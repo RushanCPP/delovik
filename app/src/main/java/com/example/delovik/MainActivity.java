328e4a7be11d749f364400e0302e4447e9e6640f
rusha <rushan.davlitshin.0809@gmail.com>
2022-07-12 23:01:24 +0500

    Реализован Schedule и его компоненты

--- app/src/main/java/com/example/delovik/MainActivity.java
+++ app/src/main/java/com/example/delovik/MainActivity.java
@@ -2,14 +2,10 @@ package com.example.delovik;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.room.Room;
-import androidx.room.RoomDatabase;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.example.delovik.backend.model.database.SchedulesDatabase;
-import com.example.delovik.backend.model.database.entity.Schedules;
 import com.example.delovik.backend.viewmodel.ScheduleViewModel;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,12 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // TEST
         ScheduleViewModel scheduleViewModel = new ViewModelProvider(this).get(ScheduleViewModel.class);
-        SchedulesDatabase database =
-                Room.databaseBuilder(
-                        getApplicationContext(),
-                        SchedulesDatabase.class,
-                        "database")
-                        .build();
+        ((TextView) findViewById(R.id.first_label)).setText(scheduleViewModel.getScheduleMutableLiveData().getValue().toString());
         // TEST
     }
 }
